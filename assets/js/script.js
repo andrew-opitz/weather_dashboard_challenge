9756818bfebc4af1e16de305507c1a254df737e8
andrew.opitz222@gmail.com <andrew.opitz@yahoo.com>
2023-10-05 12:30:17 -0400

    finished current weather need to style

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,58 +6,75 @@ const weatherBtn = $('#weather-btn')
 const outputDiv = $('#output-div')
 const weatherP = $('#weather-p')
 
-function showCurrentWeather() {
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityNameCountry}&units=imperial&appid=${apiKey}`;
-    fetch(apiUrl)
-    .then(response => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Network response was not ok');
-        }
-      })
-      .then(data => {
-          var info = data.main
-          var place = data.name
-          var feelsLike = data.main.feels_like
-          var humidity = data.main.humidity
-        var temp = data.main.temp
-        var windSpeed = data.wind.speed
-        var conditions = data.weather[0].description
-        var lat = data.coord.lat
-        var lon = data.coord.lon
-        var iconCode = ''
 
-        function getIconCode () {
-            if(conditions === 'clear sky') {
-                iconCode = '01d'
-            } else if(conditions === 'few clouds') {
-                iconCode = '02d'
-            } else if(conditions === 'scattered clouds') {
-                iconCode = '03d'
-            } else if(conditions === 'broken clouds') {
-                iconCode = '04d'
-            }
-        }
-        getIconCode()
-
-
-        const icon = new Image()
-        icon.src = `http://openweathermap.org/img/w/${iconCode}.png`
-
-        $(weatherP).append(place, temp, conditions, humidity, icon)
-        
-        // console.log(place, feelsLike, humidity, temp, windSpeed, conditions, icon, lat, lon)
-       
-    // append to input
-        // $(weatherP).append('<ul>' + '<li class="weather-list">' + place + '</li>' + '<li class="weather-list">' + temp + ' °F' + '</li>' + '<li class="weather-list">' + feelsLike + ' °F' + '</li>', '</ul>')
-      })
-      .catch(error => {
-        console.error('Fetch error:', error);
-      });
+function CurrentWeather() {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityNameCountry}&units=imperial&appid=${apiKey}`;
+  fetch(apiUrl)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Network response was not ok');
     }
-
-    document.querySelector('#weather-btn').addEventListener('click', function () {
-        cityNameCountry = userInput.val()
-        showCurrentWeather()
-      })
+  })
+  .then(data => {
+    
+    var info = data.main
+    var timezone = data.timezone
+      var place = data.name
+      var feelsLike = data.main.feels_like
+      var humidity = data.main.humidity
+      var temp = data.main.temp
+      var windSpeed = data.wind.speed
+      var conditions = data.weather[0].description
+      var iconCode = ''
+      var lat = data.coord.lat
+      var lon = data.coord.lon
+      var currentDate = new Date()
+      
+      const icon = new Image()
+    
+      if (conditions === 'clear sky') {
+        iconCode = '01d'
+      } else if (conditions === 'few clouds') {
+        iconCode = '02d'
+      } else if (conditions === 'scattered clouds') {
+        iconCode = '03d'
+      } else if (conditions === 'broken clouds') {
+        iconCode = '04d'
+      } else if (conditions === 'shower rain') {
+        iconCode = '09d'
+      } else if (conditions === 'rain') {
+        iconCode = '10d'
+      } else if (conditions === 'thunderstorm') {
+        iconCode = '11d'
+      } else if (conditions === 'snow') {
+        iconCode = '13d'
+      } else if (conditions === 'mist') {
+        iconCode = '50d'
+      }
+    
+      icon.src = `http://openweathermap.org/img/w/${iconCode}.png`
+      
+      $(weatherP).append('<ul>' +
+      '<li class="weather-list">' + place + '</li>' +
+      '<li class="weather-list">' + currentDate + '</li>' +
+      '<li class="weather-list">' + temp + ' °F' + '</li>' +
+      '<li class="weather-list" id="iconContainer">' + conditions + '</li>' +
+      '<li class="weather-list">' + humidity + ' %' + '</li>' +
+      '<li class="weather-list">' + windSpeed + ' mph' + '</li>' +
+      '</ul>');
+      
+      $('#iconContainer').append(icon)
+      
+      
+    })
+    .catch(error => {
+      console.error('Fetch error:', error);
+    });
+  }
+  
+  document.querySelector('#weather-btn').addEventListener('click', function () {
+    cityNameCountry = userInput.val()
+    CurrentWeather()
+  })
